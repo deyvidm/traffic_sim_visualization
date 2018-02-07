5891f02d34691ac38264bd23c95c13996e266b24
Deyvid <deyvidmitkov@gmail.com>
2018-02-07 15:39:11 -0500

    rehashed dependencies; added ability to add charts and their resepctive animations

--- animate.js
+++ animate.js
@@ -4,7 +4,24 @@ $.ajax({
   url:'http://localhost:8000/serve.php',
   method:'get',
   success:function(data) {
-    animate(data);
+    var total_duration = animate(data);
+
+    generate_graph(
+      '#acceleration_chart',
+      'time',
+      'acceleration', 
+      format_data_set(data[0], data[1], 'acceleration', '#ff7f0e'),
+      total_duration
+    );
+
+    generate_graph(
+      '#velocity_chart',
+      'time',
+      'velocity', 
+      format_data_set(data[0], data[2], 'velocity', '#ff7f0e'),
+      total_duration
+    );
+
   },
   error:function(data){
     alert("error");
@@ -16,24 +33,24 @@ function animate(data){
   follower_pos = data[6].slice();
 
   var size_scaler = 6;
-  var duration = 250;
+  var transition_duration = 250;
 
-  
   
   var canvas_width = Math.max.apply(null, leader_pos) + 50;
   var canvas_height = 50;
   $("#canvas").attr('width',canvas_width*size_scaler);
+  $(".chart").attr('width',canvas_width*size_scaler);
   $("#canvas").attr('height',canvas_height*size_scaler);
   
 
   var leader_translations = [];
   var follower_translations = [];
   for (var i = 0; i < follower_pos.length; i++) {  
-    leader_translations.push({value: leader_pos[i]*size_scaler, duration: duration});
-    follower_translations.push({value: follower_pos[i]*size_scaler, duration: duration});
+    leader_translations.push({value: leader_pos[i]*size_scaler, duration: transition_duration});
+    follower_translations.push({value: follower_pos[i]*size_scaler, duration: transition_duration});
   }
-  leader_translations[0].duration = 0;
-  follower_translations[0].duration = 0;
+  leader_translations[0].transition_duration = 0;
+  follower_translations[0].transition_duration = 0;
 
   var length = 4;
   var width = 2
@@ -46,26 +63,31 @@ function animate(data){
                 .attr('height',width*size_scaler)
                 .attr({ fill: '#3ccb3e' })
                 // .attr({x: follower_translations[0].value })
+  
   anime({
     targets: '#leader',
     translateX: leader_translations,
-    easing: 'linear'
+    easing: 'linear',
+    autoplay: false
   });
 
   anime({
     targets: '#follower',
     translateX: follower_translations,
-    easing: 'linear'
+    easing: 'linear',
+    autoplay: false
   });
 
-  anime({
-    target: '#buffer',
-    translateX: leader_translations.map(function (val){
-      val.value = val.value + (width*size_scaler);
-      return val;
-    }),
-  })
+  // anime({
+  //   target: '#buffer',
+  //   translateX: leader_translations.map(function (val){
+  //     val.value = val.value + (width*size_scaler);
+  //     return val;
+  //   }),
+  //   autoplay: false
+  // })
 
+  return transition_duration * leader_translations.length;
 
   // anime({
   //   targets: '#m10',
---
+++ chart.js
@@ -0,0 +1,93 @@
+function generate_graph(elem_selector, x_axis_label, y_axis_label, data, total_duration)
+{
+  nv.addGraph(function() {
+    var chart = nv.models.lineChart()
+                  .margin({left: 100, right: 100})  //Adjust chart margins to give the x-axis some breathing room.
+                  .showLegend(true)       //Show the legend, allowing users to turn on/off line series.
+                  .showYAxis(true)        //Show the y-axis
+                  .showXAxis(true)        //Show the x-axis
+    ;
+
+    chart.xAxis
+        .axisLabel(x_axis_label)
+        .tickFormat(d3.format('.02f'))
+        .ticks(20)
+
+    chart.yAxis
+        .axisLabel(y_axis_label)
+        .tickFormat(d3.format('.02f'))
+
+    // Done setting the chart up? Time to render it!
+    d3.select(elem_selector)    // Select the <svg> element you want to render the chart in.   
+        .datum(data)                    // Populate the <svg> element with chart data...
+        .call(chart)                    // Finally, render the chart!
+        .on('end', follow(elem_selector, total_duration))            // Now that the chart is rendered and ready, initiate the follow cubelet 
+        
+    nv.utils.windowResize(function() { chart.update() });
+
+    return chart;
+  });
+}
+
+function format_data_set(x_vals, y_vals, key, colour) {
+  if (x_vals.length != y_vals.length) {
+    return null;
+  }
+  values = []
+  for (var i = 0; i < x_vals.length; i++) {
+    values.push({x: x_vals[i], y: y_vals[i]});
+  }
+  return [
+  {
+    values: values,
+    key: key,
+    color: colour // THERE IS ONLY ONE RIGHT SPELLING, AND IT USES 'u'
+  }]
+}
+
+function follow(elem_selector, total_duration)
+{
+  // add the follower cube to the DOM 
+  $(elem_selector).after($('<div class="follower-cube"></div>'));
+
+  var cube_selector = elem_selector + " + .follower-cube";
+  var path_selector = elem_selector + '> g > g > g.nv-linesWrap.nvd3-svg > g > g > g.nv-groups > g > path';
+
+  // position the follower cube on top of its path
+  path_position = get_start_of_path(elem_selector, path_selector);
+  
+  var top = $(elem_selector).offset().top + path_position.y - $(cube_selector).height()/2;
+  var left = $(elem_selector).offset().left + path_position.x - $(cube_selector).width()/2;
+
+  console.log(top.toString() + "px");
+  
+  $(cube_selector)
+            .css('top', top.toString()+'px')
+            .css('left', left.toString()+'px');
+
+  // let em loose!  
+  var path = anime.path(path_selector);   
+  var motionPath = anime({
+    targets: cube_selector,
+    translateX: path('x'),
+    translateY: path('y'),
+    rotate: path('angle'),
+    easing: 'linear',
+    duration: total_duration,
+    autoplay: false
+  });
+}
+
+function get_start_of_path(path_container_selector, path_selector)
+{
+  var froot = document.querySelector(path_container_selector);
+  var path = document.querySelector(path_selector);
+  console.log(path);
+  var point = froot.createSVGPoint();
+  point.x = 0;  // replace this with the x co-ordinate of the path segment
+  point.y = 0;  // replace this with the y co-ordinate of the path segment
+  var matrix = path.getTransformToElement(froot);
+  var position = point.matrixTransform(matrix);
+
+  return position;
+}
